dd7e14ec4f7fe79068f13c98c14d9ea093db5208
nickhsine <nick@nickhsinedeMacBook-Pro.local>
2023-10-30 00:00:14 +0800

    chore(ecg): bump version to 0.0.6

--- packages/embed-code-generator/dev/mocks/index.js
+++ packages/embed-code-generator/dev/mocks/index.js
@@ -1,27 +1,9 @@
-import droppingText from './dropping-text.js'
 import dualSlides from './dual-slides/index.js'
-import electionWidgetsSeatChart from './election-widgets-seat-chart/index.js'
-import electionWidgetsVotesComparison from './election-widgets-votes-comparison/index.js'
 import karaoke from './karaoke/index.js'
-import liveBlog from './live-blog/index.js'
-import timeline from './timeline/index.js'
-import questionnaire from './questionnaire/index.js'
-import randomTextSelector from './random-text-selector.js'
 import threeStoryPoints from './three-story-points/index.js'
-import theatre from './theatre/index.js'
-import react360 from './react-360.js'
 
 export default {
-  'react-dropping-text': droppingText,
   'react-dual-slides': dualSlides,
-  'react-election-widgets-seat-chart': electionWidgetsSeatChart,
-  'react-election-widgets-votes-comparison': electionWidgetsVotesComparison,
   'react-karaoke': karaoke,
-  'react-live-blog': liveBlog,
-  'react-timeline': timeline,
-  'react-questionnaire': questionnaire,
-  'react-random-text-selector': randomTextSelector,
   'react-three-story-points': threeStoryPoints,
-  'react-theatre': theatre,
-  'react-360': react360,
 }
