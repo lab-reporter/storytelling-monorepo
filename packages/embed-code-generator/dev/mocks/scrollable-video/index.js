9334b6d521357af1045dbf3a7e367626f68432dd
nickhsine <nick@nickhsinedeMacBook-Pro.local>
2023-12-04 13:33:51 +0800

    chore(ecg): add scrollable-video mock data

---
+++ packages/embed-code-generator/dev/mocks/scrollable-video/index.js
@@ -0,0 +1,89 @@
+export default {
+  video: {
+    sources: [
+      {
+        type: 'video/mp4',
+        src: 'http://localhost:8080/mocks/scrollable-video/2020092015-tnrail-01-1280.mp4',
+        width: 1280,
+        height: 720,
+      },
+      {
+        type: 'video/mp4',
+        src: 'http://localhost:8080/mocks/scrollable-video/2020091916-tnrail-01-1440.mp4',
+        width: 1440,
+        height: 810,
+      },
+      {
+        type: 'video/mp4',
+        src: 'http://localhost:8080/mocks/scrollable-video/2020092015-tnrail-01-land-1920.mp4',
+        width: 1920,
+        height: 1080,
+      },
+      {
+        type: 'video/mp4',
+        src: 'http://localhost:8080/mocks/scrollable-video/2020091916-tnrail-01-port-720.mp4',
+        width: 720,
+        height: 1080,
+      },
+      {
+        type: 'video/mp4',
+        src: 'http://localhost:8080/mocks/scrollable-video/2020091916-tnrail-01-port-960.mp4',
+        width: 960,
+        height: 1440,
+      },
+    ],
+  },
+  captions: [
+    {
+      rawContentState: {
+        blocks: [
+          {
+            key: '9nfjs',
+            data: {},
+            text: '這是2020年9月，站在台南市「開元陸橋」上向北望見的景象。',
+            type: 'unstyled',
+            depth: 0,
+            entityRanges: [],
+            inlineStyleRanges: [],
+          },
+        ],
+        entityMap: {},
+      },
+      startTime: 0.5,
+    },
+    {
+      rawContentState: {
+        blocks: [
+          {
+            key: '9nfjs',
+            data: {},
+            text: '鐵路右側，為施工中的鐵路地下化工程。',
+            type: 'unstyled',
+            depth: 0,
+            entityRanges: [],
+            inlineStyleRanges: [],
+          },
+        ],
+        entityMap: {},
+      },
+      startTime: 2.3,
+    },
+    {
+      rawContentState: {
+        blocks: [
+          {
+            key: '9nfjs',
+            data: {},
+            text: '如今的空地，過去曾是許多人的家。',
+            type: 'unstyled',
+            depth: 0,
+            entityRanges: [],
+            inlineStyleRanges: [],
+          },
+        ],
+        entityMap: {},
+      },
+      startTime: 6.5,
+    },
+  ],
+}
