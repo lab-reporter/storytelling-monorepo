91ad73cfaa94f774fc6f5b947812eed7e3917744
nickhsine <nick@nickhsinedeMacBook-Pro.local>
2023-12-06 12:43:41 +0800

    chore(ecg): update karaoke mock data

--- packages/embed-code-generator/dev/mocks/karaoke/index.js
+++ packages/embed-code-generator/dev/mocks/karaoke/index.js
@@ -1,6 +1,6 @@
 export default {
   audioUrls: ['http://localhost:8080/mocks/karaoke/audio-example-1.mp3'],
-  textArr: [
+  quoteArr: [
     '我很確定我一回去就會被關，大概會關十幾年。',
     '但是…雖然這樣子，我想可以死在香港，',
     '對…我覺得，我要死在我的故鄉，',
