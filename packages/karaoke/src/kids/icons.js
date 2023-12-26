3db5e3ba72c7e8fc0278328d91e33278b798fe10
nickhsine <nick@nickhsinedeMacBook-Pro.local>
2023-12-27 00:44:48 +0800

    refactor(karaoke): update icons.js

--- packages/karaoke/src/kids/icons.js
+++ packages/karaoke/src/kids/icons.js
@@ -1,35 +1,62 @@
 import React/* eslint-disable-line */ from 'react'
 
+const iconURLPath =
+  'https://www.unpkg.com/@story-telling-reporter/react-karaoke/public/icons/kids'
+
 export function SoundIcon({ className, onClick, style }) {
   return (
-    <img
-      className={className}
-      src="https://www.unpkg.com/@story-telling-reporter/react-karaoke/public/icons/kids/sound.svg"
-      style={style}
-      width="100%"
-      onClick={onClick}
-    />
+    <>
+      <link rel="preload" href={`${iconURLPath}/sound.svg`} as="image" />
+      <img
+        className={className}
+        src={`${iconURLPath}/sound.svg`}
+        style={style}
+        width="100%"
+        onClick={onClick}
+      />
+    </>
   )
 }
 
 export function MuteIcon({ className, onClick, styles }) {
   return (
-    <img
-      className={className}
-      src="https://www.unpkg.com/@story-telling-reporter/react-karaoke/public/icons/kids/mute.svg"
-      styles={styles}
-      width="100%"
-      onClick={onClick}
-    />
+    <>
+      <link rel="preload" href={`${iconURLPath}/mute.svg`} as="image" />
+      <img
+        className={className}
+        src={`${iconURLPath}/mute.svg`}
+        styles={styles}
+        width="100%"
+        onClick={onClick}
+      />
+    </>
+  )
+}
+
+export function PlayIcon({ className, onClick, styles }) {
+  return (
+    <>
+      <link rel="preload" href={`${iconURLPath}/play.svg`} as="image" />
+      <img
+        className={className}
+        src={`${iconURLPath}/play.svg`}
+        styles={styles}
+        width="100%"
+        onClick={onClick}
+      />
+    </>
   )
 }
 
 export function LogoIcon({ className }) {
   return (
-    <img
-      className={className}
-      src="https://www.unpkg.com/@story-telling-reporter/react-karaoke/public/icons/kids/karaoke-logo.svg"
-      width="100%"
-    />
+    <>
+      <link rel="preload" href={`${iconURLPath}/karaoke-logo.svg`} as="image" />
+      <img
+        className={className}
+        src={`${iconURLPath}/karaoke-logo.svg`}
+        width="100%"
+      />
+    </>
   )
 }
