9596470ddbbfd0ec859b1dbc734ca92b0aaf4fde
nickhsine <nick@nickhsinedeMacBook-Pro.local>
2024-01-04 15:08:44 +0800

    feat(karaoke): named export `TwreporterKaraoke`

--- packages/karaoke/src/index.js
+++ packages/karaoke/src/index.js
@@ -1,7 +1,9 @@
 import { Karaoke as KidsKaraoke } from './kids/index'
+import { Karaoke as TwreporterKaraoke } from './twreporter/index'
 
-export { KidsKaraoke }
+export { KidsKaraoke, TwreporterKaraoke }
 
 export default {
   KidsKaraoke,
+  TwreporterKaraoke,
 }
