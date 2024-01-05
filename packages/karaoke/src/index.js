e1c1cbc0fcbda15ace63bffae73ef2e98f8a782d
nickhsine <nick@nickhsinedeMacBook-Pro.local>
2024-01-05 12:12:10 +0800

    feat(karaoke): named export `Karaoke`

--- packages/karaoke/src/index.js
+++ packages/karaoke/src/index.js
@@ -1,9 +1,32 @@
 import { Karaoke as KidsKaraoke } from './kids/index'
 import { Karaoke as TwreporterKaraoke } from './twreporter/index'
 
-export { KidsKaraoke, TwreporterKaraoke }
+export { Karaoke, KidsKaraoke, TwreporterKaraoke }
 
 export default {
+  Karaoke,
   KidsKaraoke,
   TwreporterKaraoke,
+}
+
+/**
+ *  @typedef {import('./kids/index.js').KaraokeProps} KidsProps
+ *  @typedef {import('./twreporter/index.js').KaraokeProps} TwreporterProps
+ */
+
+/**
+ *  @param {Object} props
+ *  @param {string} props.componentTheme - 'kids' or 'twreporter'
+ *  @param {...(KidsProps|TwreporterProps)} ...restProps
+ *  @returns {React.ReactNode}
+ */
+function Karaoke({ componentTheme, ...restProps }) {
+  switch (componentTheme) {
+    case 'kids':
+      return <KidsKaraoke {...restProps} />
+    case 'twreporter':
+    default: {
+      return <TwreporterKaraoke {...restProps} />
+    }
+  }
 }
