663493443f6f9ae1b7ad4f7555a92b2adfa7b765
nickhsine <nick@nickhsinedeMacBook-Pro.local>
2024-01-05 14:21:56 +0800

    chore(ecg): update karaoke mock data

--- packages/embed-code-generator/dev/mocks/karaoke/index.js
+++ packages/embed-code-generator/dev/mocks/karaoke/index.js
@@ -1,12 +1,17 @@
 export default {
-  audioUrls: ['http://localhost:8080/mocks/karaoke/audio-example-1.mp3'],
-  quoteArr: [
-    '我很確定我一回去就會被關，大概會關十幾年。',
-    '但是…雖然這樣子，我想可以死在香港，',
-    '對…我覺得，我要死在我的故鄉，',
-    '對，所以就這樣子啦，',
-    '不是台灣有什麼好與不好的問題，',
-    '是我忘記不了香港。',
-  ],
-  imgSrc: 'http://localhost:8080/mocks/karaoke/img-1.png',
+  componentTheme: 'twreporter',
+  audioUrls: ['http://localhost:8080/mocks/karaoke/audio-twreporter-1.mp3'],
+  webVtt: `WEBVTT
+
+00:00:00.000 --> 00:00:04.500
+演員，我覺得他就是一個生活的體驗者,
+
+00:00:04.600 --> 00:00:07.500
+然後生命的實踐家;
+
+00:00:08.000 --> 00:00:11.000
+期許自己啦，可以這麼做。
+`,
+  quoteBy:
+    '──史賓塞伯爵（Charles Spencer, 9th Earl Spencer），黛安娜王妃（Diana, Princess of Wales）之弟',
 }
