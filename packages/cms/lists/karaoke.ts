5f5196aff9b13d7f29fd4a422c8e221e5e62386f
nickhsine <nick@nickhsinedeMacBook-Pro.local>
2024-01-05 14:50:29 +0800

    feat(cms): generate twreporter or kids theme karaoke embed code

--- packages/cms/lists/karaoke.ts
+++ packages/cms/lists/karaoke.ts
@@ -3,21 +3,9 @@ import config from '../config'
 import embedCodeGen from '@story-telling-reporter/react-embed-code-generator'
 // @ts-ignore: no definition
 import { list, graphql } from '@keystone-6/core'
-import { checkbox, text, image, file, virtual } from '@keystone-6/core/fields'
+import { checkbox, text, file, virtual, select } from '@keystone-6/core/fields'
 
 const embedCodeWebpackAssets = embedCodeGen.loadWebpackAssets()
-
-type Session = {
-  data: {
-    id: string
-    role: string
-  }
-}
-
-function imageFileACL({ session }: { session: Session }) {
-  const fieldMode = session?.data?.role == 'contributor' ? 'hidden' : 'edit'
-  return fieldMode
-}
 
 const listConfigurations = list({
   fields: {
@@ -25,37 +13,45 @@ const listConfigurations = list({
       label: 'Karaoke 名稱',
       validation: { isRequired: true },
     }),
+    webVtt: text({
+      label: '字幕（WebVTT 格式）',
+      ui: {
+        displayMode: 'textarea',
+      },
+      defaultValue: `WEBVTT
+00:00:00.000 --> 00:00:04.500
+演員，我覺得他就是一個生活的體驗者,
+
+00:00:04.600 --> 00:00:07.500
+然後生命的實踐家;
+
+00:00:08.000 --> 00:00:11.000
+期許自己啦，可以這麼做。
+`,
+    }),
     quote: text({
       label: '引言',
       validation: { isRequired: true },
       ui: {
-        displayMode: 'textarea',
+        itemView: {
+          fieldMode: 'hidden',
+        },
+        createView: {
+          fieldMode: 'hidden',
+        },
+        listView: {
+          fieldMode: 'hidden',
+        },
       },
     }),
     audio: file({
       storage: 'files',
-      //external users can't upload files to our GCS. They can only use the image from their sources.
-      ui: {
-        createView: { fieldMode: imageFileACL },
-        itemView: { fieldMode: imageFileACL },
-      },
     }),
-    //external users can't upload files to our GCS. They can only use the image from their sources.
-    imageFile: image({
-      storage: 'images',
-      ui: {
-        createView: { fieldMode: imageFileACL },
-        itemView: { fieldMode: imageFileACL },
-      },
-    }),
-    audioLink: text(),
-    imageLink: text(),
     muteHint: checkbox({
       label: '是否產生「聲音播放提示」的 embed code',
       defaultValue: false,
     }),
-    /*
-    alignCenter: select({
+    theme: select({
       label: '哪裡要使用 embed code？',
       options: [
         {
@@ -64,37 +60,24 @@ const listConfigurations = list({
         },
         {
           label: '少年報導者文章頁',
-          value: 'kids-reporter',
+          value: 'kids',
         },
-        {
-          label: '其他網站',
-          value: 'others',
-        }
       ],
       defaultValue: 'twreporter',
     }),
-    */
     embedCode: virtual({
       label: 'Karaoke embed code',
       field: graphql.field({
         type: graphql.String,
         resolve: async (item: Record<string, unknown>): Promise<string> => {
-          const audioSrc =
-            (item?.audioLink && `${item.audioLink}`) ||
-            (item?.audio_filename &&
-              `${config.googleCloudStorage.origin}/files/${item?.audio_filename}`)
-          const imgSrc =
-            (item?.imageLink && `${item.imageLink}`) ||
-            (item?.imageFile_id &&
-              `${config.googleCloudStorage.origin}/images/${item.imageFile_id}.${item.imageFile_extension}`)
+          const audioSrc = `${config.googleCloudStorage.origin}/files/${item?.audio_filename}`
 
           const code = embedCodeGen.buildEmbeddedCode(
             'react-karaoke',
             {
+              componentTheme: item?.theme,
               audioUrls: [audioSrc],
-              quoteArr:
-                typeof item?.quote === 'string' && item.quote.split('\n'),
-              imgSrc,
+              webVtt: item?.webVtt,
             },
             embedCodeWebpackAssets
           )
