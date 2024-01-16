594026f66edd894717c8239cb7308ecab7f66f6b
nickhsine <nick@nickhsinedeMacBook-Pro.local>
2024-01-16 11:45:34 +0800

    chore(karaoke): update dev/entry.js

--- packages/karaoke/dev/entry.js
+++ packages/karaoke/dev/entry.js
@@ -13,17 +13,20 @@ const mocks = [
     audioUrls: ['./audio-twreporter-1.mp3'],
     webVtt: `WEBVTT
 
-00:00:00.000 --> 00:00:04.500
-演員，我覺得他就是一個生活的體驗者,
+00:00:00.000 --> 00:00:00.500
+演員，
+
+00:00:00.500 --> 00:00:04.000
+我覺得他就是一個生活的體驗者,
 
 00:00:04.600 --> 00:00:07.500
 然後生命的實踐家;
 
-00:00:08.000 --> 00:00:11.000
+00:00:08.000 --> 00:00:10.500
 期許自己啦，可以這麼做。
 `,
     quoteBy:
-      '──史賓塞伯爵（Charles Spencer, 9th Earl Spencer），黛安娜王妃（Diana, Princess of Wales）之弟',
+      '史賓塞伯爵（Charles Spencer, 9th Earl Spencer），黛安娜王妃（Diana, Princess of Wales）之弟',
     imgSrc: '',
   },
 ]
