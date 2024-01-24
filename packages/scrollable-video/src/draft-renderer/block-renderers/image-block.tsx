0dc3d4c3e5d229944d757aeb2ddbf14ae50742f5
nickhsine <nick@nickhsinedeMacBook-Pro.local>
2024-01-24 14:36:29 +0800

    style(sv): color and margin of image atomic block

--- packages/scrollable-video/src/draft-renderer/block-renderers/image-block.tsx
+++ packages/scrollable-video/src/draft-renderer/block-renderers/image-block.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import styled from '../../styled-components'
 
 const Figure = styled.figure`
+  /* clear browser default styles */
+  margin: 0;
+
   width: 100%;
 `
 
@@ -9,7 +12,10 @@ const FigureCaption = styled.figcaption`
   font-size: 14px;
   font-weight: 400;
   line-height: 21px;
-  color: #4a4a4a;
+  color: #666;
+
+  /* clear browser default styles */
+  margin: 8px 0 0 0;
 `
 
 const Img = styled.img`
