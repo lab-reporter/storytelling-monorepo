69764ded63879f3d1b6abb49b5143f341596351f
nickhsine <nick@nickhsinedeMacBook-Pro.local>
2024-01-25 12:03:02 +0800

    feat(sv): add v1 component

---
+++ packages/scrollable-video/src/v1/index.tsx
@@ -0,0 +1,6 @@
+// @ts-ignore there is no type definition
+import _ScrollableVideo from '@twreporter/scrollable-video'
+
+const ScrollableVideo = _ScrollableVideo.Component
+
+export { ScrollableVideo }
