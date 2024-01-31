4896787366fe2ef1bfd7773b16c40db6b706c94b
nickhsine <nick@nickhsinedeMacBook-Pro.local>
2024-01-31 16:54:54 +0800

    refactor(sv)!: update src/index.ts. Change exports

--- packages/scrollable-video/src/index.ts
+++ packages/scrollable-video/src/index.ts
@@ -1,9 +1,5 @@
 import { ScrollableVideo } from './v2/index'
-import { CaptionEditor } from './editor/index'
-
-export { CaptionEditor, ScrollableVideo }
 
 export default {
-  CaptionEditor,
   ScrollableVideo,
 }
