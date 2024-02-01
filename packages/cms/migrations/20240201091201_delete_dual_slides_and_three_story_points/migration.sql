bda0ff0242fd29c0b9eb7f3b28f6813407c77653
nickhsine <nick@nickhsinedeMacBook-Pro.local>
2024-02-01 17:12:43 +0800

    chore(cms): update schema.(graphql,prisma) and add migration script for hiding DualSlides and ThreeStoryPoints

---
+++ packages/cms/migrations/20240201091201_delete_dual_slides_and_three_story_points/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the `DualSlide` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `ThreeStoryPoint` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "DualSlide";
+
+-- DropTable
+DROP TABLE "ThreeStoryPoint";
