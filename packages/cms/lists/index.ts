709f6a38c66f1475a647e706a39602a9d03ddfe3
nickhsine <nick@nickhsinedeMacBook-Pro.local>
2024-02-01 16:57:26 +0800

    refactor(cms)!: hide ThreeStoryPoints and DualSlides list

--- packages/cms/lists/index.ts
+++ packages/cms/lists/index.ts
@@ -4,8 +4,8 @@ import Video from './video'
 import Photo from './photo'
 import Karaoke from './karaoke'
 import SubtitledAudio from './subtitled-audio'
-import ThreeStoryPoints from './three-story-points'
-import DualSlides from './dual-slides'
+// import ThreeStoryPoints from './three-story-points'
+// import DualSlides from './dual-slides'
 import ScrollableVideo from './scrollable-video'
 // import Helper from './helper'
 
@@ -16,7 +16,7 @@ export const listDefinition = {
   Photo,
   Karaoke,
   SubtitledAudio,
-  ThreeStoryPoint: ThreeStoryPoints,
-  DualSlide: DualSlides,
+  //ThreeStoryPoint: ThreeStoryPoints,
+  //DualSlide: DualSlides,
   ScrollableVideo,
 }
