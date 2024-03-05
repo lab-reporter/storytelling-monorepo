14c722c0720145ad3bcfb92f7e204142efeeeb3e
nickhsine <nick@nickhsinedeMacBook-Pro.local>
2024-03-05 22:49:01 +0800

    refactor(cms): update scrollable-video list.

--- packages/cms/lists/scrollable-video.ts
+++ packages/cms/lists/scrollable-video.ts
@@ -73,6 +73,15 @@ const listConfigurations = list({
       label: '客製化 CSS',
       defaultValue: `
 .scrollable-video {
+  /* 將捲動式影片往上移動，使其與上一個區塊連接在一起。*/
+  /* 使用情境範例：想要兩個捲動式影片連在一起，讓第二個捲動式影片與第一個影片沒有間隔。 */
+  /* 刪除下方註解即可使用。 */
+  /*
+  &.container {
+    margin-top: -40px;
+  } 
+  */
+
   /* 將捲動式影片向左移動，撐滿文章頁 */
   @media (max-width: 767px) {
     &.container {
