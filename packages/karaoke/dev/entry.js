b1ee347b43e68a727312fe2d3f150918c0ef556d
nickhsine <nickhsine97753017@gmail.com>
2024-03-15 10:22:43 +0800

    chore(karaoke): update dev/entry.js. Add text block

--- packages/karaoke/dev/entry.js
+++ packages/karaoke/dev/entry.js
@@ -38,10 +38,31 @@ const MockContentBlock = styled.div`
   margin-top: 50px;
 `
 
+const TextContentBlock = styled.p`
+  margin-left: auto;
+  margin-right: auto;
+
+  width: 580px;
+
+  @media (min-width: 1024px) and (max-width: 1440px) {
+    width: 480px;
+  }
+
+  color: rgb(64, 64, 64);
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 2.11;
+  letter-spacing: 0.6px;
+  white-space: pre-wrap;
+`
+
 root.render(
   <div>
     <TwreporterKaraoke hintOnly={true} />
     <MockContentBlock />
+    <TextContentBlock>
+      野聲另一名研究員，同樣研究台灣黑熊超過10年的蔡幸蒨，近年在台灣黑熊救傷案例最多的花蓮卓溪鄉架設了自動相機，3年內拍下23隻不同黑熊在此活動，他們訪談了51名族人，67%有目擊黑熊經驗，不過人熊遭遇時有74%的熊會跑掉，出現威嚇動作者不到5%，都是因母熊帶小熊，而且與人距離在5公尺內。
+    </TextContentBlock>
     {mocks.map((mock, index) => {
       return (
         <div key={index}>
