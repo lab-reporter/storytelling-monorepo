9fc5abdcabf6306680ed0725a32f1bf2ff69c280
nickhsine <nickhsine97753017@gmail.com>
2024-08-08 23:21:49 +0800

    chore(tsc): add types/index.d.ts and add `types` in package.json

---
+++ packages/three-story-controls/types/index.d.ts
@@ -0,0 +1,19 @@
+// index.d.ts
+
+import React from 'react'; // eslint-disable-line
+import { CameraData, POI } from '../src/react-components/type'
+import { GTLFModelObject } from '../src/react-components/loading-progress'
+
+// Type definitions for CameraHelperProps
+export type CameraHelperProps = {
+  modelObjs: GTLFModelObject[]
+  pois?: POI[]
+  onChange?: (arg: CameraData) => void
+}
+
+// Component definitions
+export function CameraHelper({
+  modelObjs,
+  pois,
+  onChange,
+}: CameraHelperProps): JSX.Element
