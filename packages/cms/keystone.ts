f74ad970b578d0553c9474e3678a932e1076b18b
nickhsine <nickhsine97753017@gmail.com>
2024-08-14 12:08:34 +0800

    refactor(cms): update keystone.ts. Add scrollable-three-model demo page

--- packages/cms/keystone.ts
+++ packages/cms/keystone.ts
@@ -105,20 +105,31 @@ export default withAuth(
           res.redirect('/signin')
         }
 
-        // ThreeJS router
-        app.use(
-          '/three',
-          // Serve static files, including js, css and html
-          // BTW, the reason we use `process.cwd()` rather than `__dirname`
-          // is because `__dirname` won't return the correct absolute path;
-          // it return a wrong relative path `../..`.
-          // I think it is a bug for `@keystone/core`.
-          express.static(path.resolve(process.cwd(), './public/three'))
-        )
-
         app.use(
           '/static',
           express.static(path.resolve(process.cwd(), './static'))
+        )
+
+        app.get(
+          '/demo/scrollable-three-models/:id',
+          authenticationMw,
+          async (req, res) => {
+            const itemId = req.params.id
+
+            const context = await commonContext.withRequest(req, res)
+            const item = await context.query.ScrollableThreeModel.findOne({
+              where: { id: itemId },
+              query: 'embedCode',
+            })
+
+            if (!item) {
+              return res
+                .status(404)
+                .send(`ScrollableThreeModel ${itemId} is not found`)
+            }
+
+            res.send(renderScrollableThreeModelHtml(item?.embedCode))
+          }
         )
 
         app.get(
@@ -221,6 +232,28 @@ const renderScrollableVideoHtml = (html: string) => {
   <div class="article-container">
     ${html}
   </div>
+</body>
+</html>
+`
+}
+
+const renderScrollableThreeModelHtml = (html: string) => {
+  return `
+<html>
+<head>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style> 
+  * { box-sizing: border-box; } 
+  body { margin:0; padding:50vh 0; font-family: 'Roboto', 'Noto Sans TC', sans-serif } 
+  </style>
+</head>
+<body>
+<div>
+${html}
+</div>
 </body>
 </html>
 `
