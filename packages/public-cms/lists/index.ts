185e74c59abff0bc166a939a20613b7f47bc5e2f
nickhsine <nickhsine97753017@gmail.com>
2024-08-30 00:19:39 +0800

    refactor(public-cms): add lists/index.ts

---
+++ packages/public-cms/lists/index.ts
@@ -0,0 +1,11 @@
+import User from './user'
+import ScrollToAudio from './scroll-to-audio'
+import ScrollableThreeModel from './scrollable-three-model'
+import ScrollableVideo from './scrollable-video'
+
+export const listDefinition = {
+  User,
+  ScrollToAudio,
+  ScrollableThreeModel,
+  ScrollableVideo,
+}
