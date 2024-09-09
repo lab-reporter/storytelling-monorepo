2d1063197508ae4c38dafda722594ecadb463de1
nickhsine <nickhsine97753017@gmail.com>
2024-09-09 23:44:10 +0800

    refactor(public-cms): update lists/scrollable-video.ts. Remove unwanted custom CSS

--- packages/public-cms/lists/scrollable-video.ts
+++ packages/public-cms/lists/scrollable-video.ts
@@ -49,40 +49,6 @@ const listConfigurations = list({
       label: '客製化 CSS',
       defaultValue: `
 .scrollable-video {
-  /* 將捲動式影片往上移動，使其與上一個區塊連接在一起。*/
-  /* 使用情境範例：想要兩個捲動式影片連在一起，讓第二個捲動式影片與第一個影片沒有間隔。 */
-  /* 刪除下方註解即可使用。 */
-  /*
-  &.container {
-    margin-top: -40px;
-  } 
-  */
-
-  /* 將捲動式影片向左移動，撐滿文章頁 */
-  @media (max-width: 767px) {
-    &.container {
-      margin-left: -3.4vw;
-    }
-  }
-
-  @media (min-width: 768px) and (max-width: 1023px) {
-    &.container {
-      margin-left: calc((100vw - 512px)/2 * -1);
-    }
-  }
-
-  @media (min-width: 1024px) and (max-width: 1439px) {
-    &.container {
-      margin-left: calc((100vw - 550px)/2 * -1);
-    }
-  }
-
-  @media (min-width: 1440px) {
-    &.container {
-      margin-left: calc((100vw - 730px)/2 * -1);
-    }
-  }
-
   /* 覆寫所有區塊預設的 css */
   .section {
     /* 例如：background-color: pink; */
