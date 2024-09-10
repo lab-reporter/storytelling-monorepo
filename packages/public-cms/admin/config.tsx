68fd581e989f8249e92e7f566b945a70ebd82e57
nickhsine <nickhsine97753017@gmail.com>
2024-09-10 17:12:25 +0800

    refactor(public-cms): update admin/config.tsx

--- packages/public-cms/admin/config.tsx
+++ packages/public-cms/admin/config.tsx
@@ -5,7 +5,7 @@ import { AdminConfig } from '@keystone-6/core/types'
 import { CustomNavigation } from './components/custom-navigation'
 
 function CustomLogo() {
-  return <h3>報導者新聞敘事元件庫</h3>
+  return <h3>報導者數位敘事元件庫</h3>
 }
 
 export const components: AdminConfig['components'] = {
