d2b283f6df31b4520402f5baa56df9915159c0e1
nickhsine <nickhsine97753017@gmail.com>
2024-09-16 16:27:35 +0800

    feat(sv): add `src/cms-editor/` folder. Add `ScrollableVideoEditor`

---
+++ packages/scrollable-video/src/cms-editor/mark.tsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useRef } from 'react'
+import { DeleteMarkIcon, EditMarkIcon, MarkIcon } from './styled'
+import { useState } from 'react'
+import styled from '../styled-components'
+
+const Container = styled.div`
+  position: relative;
+
+  svg {
+    width: 40px;
+  }
+
+  svg:nth-child(2) {
+    position: absolute;
+    width: 40px;
+    height: 40px;
+    bottom: -20px;
+    left: 0px;
+  }
+`
+
+export function CaptionMark({
+  className,
+  captionIdx,
+  onEdit,
+  onRemove,
+}: {
+  className?: string
+  captionIdx: number
+  onEdit: (captionIdx: number) => void
+  onRemove: (captionIdx: number) => void
+}) {
+  const [editMode, setEditMode] = useState(false)
+  const wrapperRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    /**
+     * Alert if clicked on outside of element
+     */
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        wrapperRef.current &&
+        !wrapperRef.current.contains(event.target as Node)
+      ) {
+        if (editMode) {
+          setEditMode(false)
+        }
+      }
+    }
+    // Bind the event listener
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [wrapperRef, editMode])
+
+  const iconsJsx = editMode ? (
+    <>
+      <EditMarkIcon
+        onClick={() => {
+          onEdit(captionIdx)
+        }}
+      />
+      <DeleteMarkIcon
+        onClick={() => {
+          onRemove(captionIdx)
+        }}
+      />
+    </>
+  ) : (
+    <MarkIcon
+      onClick={() => {
+        setEditMode(true)
+      }}
+    />
+  )
+
+  return (
+    <Container className={className} ref={wrapperRef}>
+      {iconsJsx}
+    </Container>
+  )
+}
--- packages/scrollable-video/src/index.tsx
+++ packages/scrollable-video/src/index.tsx
@@ -1,9 +1,15 @@
 import { ScrollableVideo } from './v2/index'
 import { ScrollableVideoForKeystoneEditorCMS } from './v2/scrollable-video-for-keystone-editor'
+import { ScrollableVideoEditor } from './cms-editor/index'
 
-export { ScrollableVideo, ScrollableVideoForKeystoneEditorCMS }
+export {
+  ScrollableVideo,
+  ScrollableVideoForKeystoneEditorCMS,
+  ScrollableVideoEditor,
+}
 
 export default {
   ScrollableVideo,
   ScrollableVideoForKeystoneEditorCMS,
+  ScrollableVideoEditor,
 }
