4c318de4c79d3a63954533c731102ceb3098e833
nickhsine <nickhsine97753017@gmail.com>
2024-09-19 14:25:42 +0800

    chroe(sv): add tpyes/index.d.ts and update package.json. Add `types` property

---
+++ packages/scrollable-video/types/index.d.ts
@@ -0,0 +1,25 @@
+import {
+  ScrollableVideo,
+  ScrollableVideoEditor,
+  ScrollableVideoForKeystoneEditorCMS,
+} from '../src/index'
+
+import { ScrollableVideoProps } from '../src/v2/index'
+
+import { ScrollableVideoEditorProps } from '../src/cms-editor/index'
+
+import { AlignmentEnum, WidthEnum, CaptionState } from '../src/type'
+import { ThemeEnum } from '../src/cms-editor/type'
+
+export type { CaptionState }
+
+export {
+  AlignmentEnum,
+  WidthEnum,
+  ThemeEnum,
+  ScrollableVideo,
+  ScrollableVideoProps,
+  ScrollableVideoEditor,
+  ScrollableVideoEditorProps,
+  ScrollableVideoForKeystoneEditorCMS,
+}
