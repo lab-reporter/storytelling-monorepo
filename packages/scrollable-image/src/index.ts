0257501a6101e90fcd37ca1eab75fa2b921809ee
nickhsine <nickhsine97753017@gmail.com>
2024-12-14 00:00:27 +0800

    refactor(si): add src/index.ts

---
+++ packages/scrollable-image/src/index.ts
@@ -0,0 +1,9 @@
+import { ScrollableImageEditor } from './cms-editor/index'
+import { ScrollableImage } from './scrollable-image'
+
+export { ScrollableImageEditor, ScrollableImage }
+
+export default {
+  ScrollableImageEditor,
+  ScrollableImage,
+}
