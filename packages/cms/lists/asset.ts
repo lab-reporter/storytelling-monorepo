2f95f49e9a1dd6bc573b2282b87497f771ffd8ff
nickhsine <nickhsine97753017@gmail.com>
2024-12-27 23:38:11 +0800

    feat(cms): add `Asset` list

---
+++ packages/cms/lists/asset.ts
@@ -0,0 +1,80 @@
+import config from '../config'
+import { graphql, list } from '@keystone-6/core'
+import { timestamp, text, file, virtual } from '@keystone-6/core/fields'
+import {
+  allowAllRoles,
+  allowRoles,
+  RoleEnum,
+} from './utils/access-control-list'
+
+const listConfigurations = list({
+  fields: {
+    name: text({
+      label: '標題',
+      validation: { isRequired: true },
+    }),
+    file: file({
+      label: '檔案',
+      storage: 'files',
+    }),
+    url: virtual({
+      label: '網址',
+      field: graphql.field({
+        type: graphql.String,
+        resolve: (item: Record<string, unknown>): string => {
+          return `${config.gcs.urlPrefix}/files/${item?.file_filename}`
+        },
+      }),
+    }),
+    description: text({
+      label: '描述',
+      ui: {
+        displayMode: 'textarea',
+      },
+    }),
+    createdAt: timestamp({
+      defaultValue: { kind: 'now' },
+      ui: {
+        itemView: {
+          fieldMode: 'read',
+        },
+      },
+    }),
+    updatedAt: timestamp({
+      db: {
+        updatedAt: true,
+      },
+      ui: {
+        itemView: {
+          fieldMode: 'read',
+        },
+      },
+    }),
+  },
+  ui: {
+    label: 'Asset（其他資源）',
+    labelField: 'name',
+    listView: {
+      initialColumns: ['name', 'url'],
+      pageSize: 50,
+    },
+  },
+
+  access: {
+    operation: {
+      query: allowAllRoles(),
+      create: allowRoles([
+        RoleEnum.Owner,
+        RoleEnum.Admin,
+        RoleEnum.Editor,
+        RoleEnum.Contributor,
+      ]),
+      update: allowRoles([RoleEnum.Owner, RoleEnum.Admin, RoleEnum.Editor]),
+      delete: allowRoles([RoleEnum.Owner, RoleEnum.Admin, RoleEnum.Editor]),
+    },
+  },
+
+  hooks: {},
+})
+
+export default listConfigurations
--- packages/cms/lists/index.ts
+++ packages/cms/lists/index.ts
@@ -2,6 +2,7 @@ import User from './user'
 import Audio from './audio'
 import Video from './video'
 import Photo from './photo'
+import Asset from './asset'
 import Karaoke from './karaoke'
 import SubtitledAudio from './subtitled-audio'
 import ScrollToAudio from './scroll-to-audio'
@@ -15,6 +16,7 @@ export const listDefinition = {
   Audio,
   Video,
   Photo,
+  Asset,
   Karaoke,
   SubtitledAudio,
   ScrollToAudio,
