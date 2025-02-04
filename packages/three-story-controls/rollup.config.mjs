9e8e7ffee5d9500a795f23c97e192f54f051fbed
nickhsine <nickhsine97753017@gmail.com>
2025-02-04 22:16:28 +0800

    feat(three-story-controls): build commonjs and esmodule codes use rollup to transpile typescript into commonjs and esmodule

---
+++ packages/three-story-controls/rollup.config.mjs
@@ -0,0 +1,25 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'lib/index.esm.js', // output esmodule
+      format: 'es',
+    },
+    {
+      file: 'lib/index.cjs.js', // output commonjs
+      format: 'cjs',
+    },
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.json',
+    }),
+  ],
+  external: ['react', 'three-story-controls', 'three', 'lodash', 'gsap', 'draft-js', 'axios', '@gsap/react', 'nanoid'] // 將不需要打包的依賴列在這裡，例如 ['react', 'lodash']
+}
