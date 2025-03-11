447b1f3f00f6f3e2746bb485ef9a1c40492a5aa9
Pin-Ching Chen <p.ching0811@gmail.com>
2025-03-11 20:14:45 +0800

    feat: add puzzle-photo-infra to the public-cms list

---
+++ packages/public-cms/lists/puzzle-photo-infra.ts
@@ -0,0 +1,106 @@
+// import { buildScrollToAudioEmbedCode } from '@story-telling-reporter/react-embed-code-generator'
+import { list, graphql } from '@keystone-6/core'
+import { text, virtual, relationship, timestamp } from '@keystone-6/core/fields'
+import { createdByFilter, createdByHooks } from './utils/access-control-list'
+
+const listConfigurations = list({
+  fields: {
+    name: text({
+      label: '拼圖照片-基礎建設',
+      validation: { isRequired: true },
+    }),
+    preview: virtual({
+      field: graphql.field({
+        type: graphql.JSON,
+        resolve(item: Record<string, unknown>): Record<string, string> {
+          return {
+            href: `/demo/scroll-to-audios/${item.id}`,
+            label: '捲到式聲音預覽',
+            buttonLabel: 'Preview',
+          }
+        },
+      }),
+      ui: {
+        // A module path that is resolved from where `keystone start` is run
+        views: './lists/views/link-button',
+        createView: {
+          fieldMode: 'hidden',
+        },
+        itemView: {
+          fieldPosition: 'sidebar',
+        },
+        listView: {
+          fieldMode: 'hidden',
+        },
+      },
+      graphql: {
+        omit: {
+          create: true,
+          update: true,
+        },
+      },
+    }),
+    created_at: timestamp({
+      label: 'Created At',
+      defaultValue: { kind: 'now' },
+      ui: {
+        createView: {
+          fieldMode: 'hidden',
+        },
+        itemView: {
+          fieldMode: 'read',
+        },
+      },
+    }),
+    updated_at: timestamp({
+      label: 'Updated At',
+      db: {
+        updatedAt: true,
+      },
+      ui: {
+        createView: {
+          fieldMode: 'hidden',
+        },
+        itemView: {
+          fieldMode: 'read',
+        },
+      },
+    }),
+    created_by: relationship({
+      ref: 'User',
+      many: false,
+      ui: {
+        createView: {
+          fieldMode: 'hidden',
+        },
+        itemView: {
+          fieldMode: 'read',
+        },
+      },
+    }),
+  },
+  ui: {
+    listView: {
+      initialSort: { field: 'id', direction: 'DESC' },
+      initialColumns: ['name'],
+      pageSize: 50,
+    },
+    labelField: 'name',
+    label: '拼圖照片-基礎建設（測試版）',
+  },
+
+  access: {
+    operation: () => true,
+    filter: createdByFilter,
+  },
+  hooks: {
+    resolveInput: (args) => {
+      if (typeof createdByHooks.resolveInput === 'function') {
+        return createdByHooks.resolveInput(args)
+      }
+      return args.resolvedData
+    },
+  },
+})
+
+export default listConfigurations
