45f402dabbb77f3a9d20b5494f53423029d4bdc7
Pin-Ching Chen <p.ching0811@gmail.com>
2025-03-11 19:30:50 +0800

    feat(puzzle-photo-infra): add puzzle-photo-infra folder

---
+++ packages/puzzle-photo-infra/dev/entry.js
@@ -0,0 +1,37 @@
+// import Karaoke from '../src/react-components'
+import React from 'react' // eslint-disable-line
+import styled from 'styled-components'
+import { createRoot } from 'react-dom/client'
+// import {  } from '../src/index'
+
+const reactRootId = 'root'
+const container = document.getElementById(reactRootId)
+const root = createRoot(container)
+
+const MockContentBlock = styled.div`
+  height: 200vh;
+  background-color: pink;
+  margin-bottom: 50px;
+  margin-top: 50px;
+`
+
+const EmbedCodeBlock = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 700px;
+`
+
+root.render(
+  <div>
+    {/* <ScrollToAudio hintOnly={true} /> */}
+    <MockContentBlock />
+    <EmbedCodeBlock>
+      {/* <ScrollToAudio id="scroll-to-audio-1" audioUrls={['./audio-1.mp3']} /> */}
+    </EmbedCodeBlock>
+    <MockContentBlock />
+    <div id="scroll-to-audio-1-bottom-entry-point" />
+    {/* <ScrollToAudio id="scroll-to-audio-2" audioUrls={['./audio-2.mp3']} /> */}
+    <MockContentBlock />
+    <div id="scroll-to-audio-2-bottom-entry-point" />
+  </div>
+)
